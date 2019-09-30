ca14f5da291c90a393fe8a0cdcc00afe88d813b2
Jardine <jardine.miller@airts.co.uk>
2019-09-30 14:23:35 +0100

    reformatting

--- Lesson 2/Project 2/huffman_coding.py
+++ Lesson 2/Project 2/huffman_coding.py
@@ -51,42 +51,45 @@ def construct_tree(freq_dict):
 
     return tuples_list[0][0]
 
+def encode_string(string, huffman_table):
+    encoded_data = ""
+
+    for char in string:
+        encoded_data += huffman_table[char]
+
+    return encoded_data
+
 def huffman_encoding(data):
     frequencies = get_frequencies_dict(data)
     tree = construct_tree(frequencies)
     huffman_table = construct_huffman_table(tree)
-    encoded_data = ""
-
-    for char in data:
-        encoded_data += huffman_table[char]
+    encoded_data = encode_string(data, huffman_table)
 
     return (encoded_data, tree)
 
-def get_char_from_tree(data_arr, tree):
+def get_char_from_tree(bit_arr, tree):
     if type(tree) is str:
         return tree
 
-    bit = data_arr.pop(0)
+    bit = bit_arr.pop(0)
     
     if bit == "0":
-        return get_char_from_tree(data_arr, tree.left)
+        return get_char_from_tree(bit_arr, tree.left)
     else:
-        return get_char_from_tree(data_arr, tree.right)
+        return get_char_from_tree(bit_arr, tree.right)
     
 
 def huffman_decoding(bit_string, tree):
     decoded_string = ""
-    data_arr = [bit for bit in bit_string]
+    bit_arr = [bit for bit in bit_string]
 
-    while len(data_arr) > 0:
-        decoded_string += get_char_from_tree(data_arr, tree)
+    while len(bit_arr) > 0:
+        decoded_string += get_char_from_tree(bit_arr, tree)
 
     return decoded_string
 
 
 if __name__ == "__main__":
-    codes = {}
-
     a_great_sentence = "The bird is the word"
 
     print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
@@ -101,7 +104,3 @@ if __name__ == "__main__":
 
     print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
     print ("The content of the encoded data is: {}\n".format(decoded_data))
-
-
-
-# from collections import defaultdict
