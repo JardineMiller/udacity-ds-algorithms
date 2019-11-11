a07a9e45ba3c7ae8b322f462a9eea358313ba4fd
Jardine <jardine.miller@gmail.com>
2019-11-11 22:04:30 +0000

    tests for dutch national flag

--- Lesson 3/Project 3/dutch-national-flag.py
+++ Lesson 3/Project 3/dutch-national-flag.py
@@ -5,37 +5,54 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    next_0 = 0
-    next_2 = len(input_list) - 1
+    min_pos = 0
+    max_pos = len(input_list) - 1
     
     i = 0
     
-    while i <= next_2:
+    while i <= max_pos:
+        if input_list[i] is None or type(input_list[i]) is not int:
+            raise ValueError("input list must contain only ints between 0 and 2")
+
+        if input_list[i] < 0 or input_list[i] > 2:
+            raise ValueError("following rules have not been met: 0 <= entry <= 2")
+
         if input_list[i] is 0:
-            input_list[i] = input_list[next_0]
-            input_list[next_0] = 0
-            next_0 += 1
+            input_list[i] = input_list[min_pos]
+            input_list[min_pos] = 0
+            min_pos += 1
             i += 1
             continue
         
         if input_list[i] is 2:
-            input_list[i] = input_list[next_2]
-            input_list[next_2] = 2
-            next_2 -= 1
+            input_list[i] = input_list[max_pos]
+            input_list[max_pos] = 2
+            max_pos -= 1
             continue
             
         i += 1
 
     return input_list
 
-def test_function(test_case):
-    sorted_array = sort_012(test_case)
-    print(sorted_array)
-    if sorted_array == sorted(test_case):
-        print("Pass")
-    else:
-        print("Fail")
+def test(scenario, result, expected):
+	separator = "-------------------------"
+	scenario = "Scenario: " + scenario
+	status = "Status: PASS" if result == expected else "Status: FAIL"
+	result_str = "Result: " + str(result)
+	expected_str = "Expected: " + str(expected)
 
-test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
-test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+	print(scenario)
+	print(result_str)
+	print(expected_str)
+	print(status)
+	print(separator)
+
+test("array 1", sort_012([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]), sorted([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]))
+test("array 2", sort_012([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]), sorted([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]))
+test("array 3", sort_012([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]), sorted([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]))
+test("empty array returns empty array", sort_012([]), [])
+
+try:
+    sort_012([0,1,2,3])
+except ValueError:
+    test("throws exception when invalid num present in array", True, True)
